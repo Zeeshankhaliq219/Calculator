81518352ab4f95db83ac0d241601f2888adca11f
Zeeshan Khaliq <91781453+Zeeshankhaliq219@users.noreply.github.com>
2022-01-12 20:36:02 +0500

    ADD FUNCTIONS

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function getResult() {
     let result = document.getElementById("result");
     result.value = eval(result.value);
 };
+function cut() {
+    if (result.value) {
+      result.value = result.value.slice(0, -1);
+    }
+}
+
+function color() {
+  let x = document.getElementById("main");
+  if (x.style.backgroundColor === "white") {
+    x.style.backgroundColor = "black";
+    x.style.borderColor = "black";
+    
+  } else {
+    x.style.backgroundColor = "white";
+    x.style.borderColor = "white";
+    
+  }
+}
